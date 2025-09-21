96b2f10fdd354e1565a84f11dbb2214356b56683
Falk Werner <falk.werner@gmx.net>
2025-09-21 11:14:21 +0200

    add admin api to inspect existing buckets
    Signed-off-by: Falk Werner <falk.werner@gmx.net>

--- src/kvs-admin-v1.php
+++ src/kvs-admin-v1.php
@@ -1,12 +1,42 @@
 <?php
 
+use KeyValueStore\Config as config;
+
 define('KVS_ADMIN_V1_PREFIX', '/admin/v1/');
 
-function kvs_store_v1_process() {
+function kvs_admin_v1_process_bucket() {
+    $method = $_SERVER['REQUEST_METHOD'];
+    switch ($method) {
+        case 'GET':
+            $conn = kvs_db_open();
+            $buckets = kvs_list_buckets($conn);
+
+            http_response_code(200);
+            header('Content-Type: application/json');
+            echo json_encode($buckets);
+            break;
+        default:
+            http_response_code(405);
+            break;
+    }
+}
+
+function kvs_admin_v1_process() {
+    $headers = getallheaders();
+    $auth = http_get_header("authorization");
+    if (!$auth) {
+        http_response_code(401);
+        return;
+    }
+    if ($auth != "Bearer " . config\ADMIN_PASSWORD) {
+        error_log("failed to access admin API: invalid token");
+        http_response_code(401);
+        return;
+    }
 
     $path = substr($_SERVER['PATH_INFO'], strlen(KVS_ADMIN_V1_PREFIX));
     if ($path == "bucket") {
-
+        kvs_admin_v1_process_bucket();
     }
     else if (preg_match("/^bucket\/([^\/]+)$/", $path, $matches)) {
         $bucket = matches[1];

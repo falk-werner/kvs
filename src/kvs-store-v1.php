4c60de3dc6c7ac9eaae91309c9941f4363411811
Falk Werner <falk.werner@gmx.net>
2025-09-21 21:09:05 +0200

    implement some store APIs
    Signed-off-by: Falk Werner <falk.werner@gmx.net>

--- src/kvs-store-v1.php
+++ src/kvs-store-v1.php
@@ -6,25 +6,26 @@ function kvs_store_v1_process_bucket($bucket_name) {
     $method = $_SERVER['REQUEST_METHOD'];
     switch ($method) {
         case 'GET':
-            http_response_code(200);
-            header('Content-Type: text/plain');
-            echo "Key1: A\n";
-            echo "Key2: A\n";
-            echo "Key3: A\n";
-            foreach (getallheaders() as $name => $value) {
-                echo "$name: $value\n";
-            }
-
             $conn = kvs_db_open();
             $bucket = kvs_bucket_by_name($conn, $bucket_name);
-            if ($bucket !== false) {
-                echo "ID: " . $bucket->id . "\n";
-                echo "NAME: " . $bucket->name . "\n";
-                echo "MAX_ENTRIES: " . $bucket->max_entries . "\n";
+            if (!$bucket) {
+                http_response_code(404);
+                return;
             }
-            else {
-                echo "bucket not found\n";
+            $entries = kvs_entry_list($conn, $bucket->id);
+            http_response_code(200);
+            header('Content-Type: application/json');
+            echo json_encode($entries, JSON_FORCE_OBJECT);
+            break;
+        case 'DELETE':
+            $conn = kvs_db_open();
+            $bucket = kvs_bucket_by_name($conn, $bucket_name);
+            if (!$bucket) {
+                http_response_code(404);
+                return;
             }
+            kvs_entry_remove_all($conn, $bucket->id);
+            http_response_code(200);
             break;
         default:
             http_response_code(405);
@@ -32,15 +33,20 @@ function kvs_store_v1_process_bucket($bucket_name) {
     }
 }
 
-function kvs_store_v1_process_keys($bucket) {
+function kvs_store_v1_process_keys($bucket_name) {
     $method = $_SERVER['REQUEST_METHOD'];
     switch ($method) {
         case 'GET':
+            $conn = kvs_db_open();
+            $bucket = kvs_bucket_by_name($conn, $bucket_name);
+            if (!$bucket) {
+                http_response_code(404);
+                return;
+            }
+            $names = kvs_entry_list_keys($conn, $bucket->id);
             http_response_code(200);
-            header('Content-Type: text/plain');
-            echo "Key1\n";
-            echo "Key2\n";
-            echo "Key3\n";
+            header('Content-Type: application/json');
+            echo json_encode($names);
             break;
         default:
             http_response_code(405);
@@ -48,7 +54,7 @@ function kvs_store_v1_process_keys($bucket) {
     }
 }
 
-function kvs_store_v1_process_value($bucket, $key) {
+function kvs_store_v1_process_entry($bucket, $key) {
     $method = $_SERVER['REQUEST_METHOD'];
     switch ($method) {
         case 'GET':
@@ -76,14 +82,14 @@ function kvs_store_v1_process() {
         $bucket = $matches[1];
         kvs_store_v1_process_bucket($bucket);
     }
-    else if (preg_match('/^\/store\/v1\/bucket\/([^\/]+)\/keys$/', $path, $match)) {
+    else if (preg_match('/^\/store\/v1\/bucket\/([^\/]+)\/keys$/', $path, $matches)) {
         $bucket = $matches[1];
         kvs_store_v1_process_keys($bucket);
     }
-    else if (preg_match('/^\/store\/v1\/bucket\/([^\/]+)\/key\/([a-zA-Z0-9-_\.]+)$/')) {
-        $bucket = $match[1];
-        $key = $match[2];
-        kvs_store_v1_process_value($bucket, $key);
+    else if (preg_match('/^\/store\/v1\/bucket\/([^\/]+)\/entry\/([a-zA-Z0-9-_\.]+)$/', $path, $matches)) {
+        $bucket = $matches[1];
+        $key = $matches[2];
+        kvs_store_v1_process_entry($bucket, $key);
     }
     else {
         http_response_code(404);

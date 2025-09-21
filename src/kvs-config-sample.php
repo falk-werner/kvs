2964eae83c29019f28e367eb82e2fec477792c7b
Falk Werner <falk.werner@gmx.net>
2025-09-21 15:48:01 +0200

    rename ADMIN_PASSWORD to ADMIN_TOKEN
    Signed-off-by: Falk Werner <falk.werner@gmx.net>

--- src/kvs-config-sample.php
+++ src/kvs-config-sample.php
@@ -8,7 +8,7 @@ const DB_PASSWORD = 'secret';
 const DB_DATABASE = 'db';
 const DB_TABLE_PREFIX = 'kvs_';
 
-const ADMIN_PASSWORD = 'secret';
+const ADMIN_TOKEN = 'secret';
 
 const JWT_SECRET = 'jwt-secret';
 
